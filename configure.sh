de0449154285585621710857308cb9292e349ef1
koiruda <koiruda@gmail.com>
2023-02-09 00:01:32 +0900

    Fix shell variables

--- configure.sh
+++ configure.sh
@@ -10,15 +10,15 @@ ln -snfv "${DOTPATH}/config/zsh/.zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
 ln -snfv "${DOTPATH}/config/zsh/.zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
 ln -snfv "${DOTPATH}/config/zsh/.p10k.zsh" "${XDG_CONFIG_HOME}/zsh/.p10k.zsh"
 
-cd "$DOTPATH"
+cd "${DOTPATH}"
 
 for f in .??*
 do
-    [ "$f" = ".git" ] && continue
+    [ "${f}" = ".git" ] && continue
 
-    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+    ln -snfv "${DOTPATH}/$f" "${HOME}/$f"
 done
 
 # Link vimrc to init.vim
 mkdir -p ~/.config/nvim
-ln -snfv $DOTPATH/.vimrc ~/.config/nvim/init.vim
+ln -snfv "${DOTPATH}/.vimrc" "${XDG_CONFIG_HOME}/nvim/init.vim"
