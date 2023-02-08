12c0cb22802c72a7a40e71ede118fff1cc8109d1
koiruda <koiruda@gmail.com>
2023-02-08 15:47:13 +0900

    Update zsh config

--- configure.sh
+++ configure.sh
@@ -3,8 +3,14 @@
 set -ue
 
 DOTPATH=~/dotfiles
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
-cd ~/dotfiles
+mkdir -p "${XDG_CONFIG_HOME}/zsh"
+ln -snfv "${DOTPATH}/config/zsh/.zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
+ln -snfv "${DOTPATH}/config/zsh/.zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
+ln -snfv "${DOTPATH}/config/zsh/.p10k.zsh" "${XDG_CONFIG_HOME}/zsh/.p10k.zsh"
+
+cd "$DOTPATH"
 
 for f in .??*
 do
@@ -13,6 +19,6 @@ do
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
 
+# Link vimrc to init.vim
 mkdir -p ~/.config/nvim
 ln -snfv $DOTPATH/.vimrc ~/.config/nvim/init.vim
-
--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ sudo apt update
 sudo apt upgrade
 sudo apt install curl git tig vim shellcheck zsh -y
 
-echo "Installing neovim..."
+echo "Installing latest neovim..."
 wget -P /tmp https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb && \
 sudo apt install /tmp/nvim-linux64.deb -y && \
 rm /tmp/nvim-linux64.deb
 
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-if [ -d "$ZINIT_HOME" ]; then
+if [ -d "${ZINIT_HOME}" ]; then
     echo "zinit is already installed."
-    git -C "$ZINIT_HOME" pull
+    git -C "${ZINIT_HOME}" pull
 else
     echo "Installing zinit..."
-    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+    git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}"
     source "${ZINIT_HOME}/zinit.zsh"
 fi
 
