482b55f8ad553f4f61188a1adf68a47ef749edea
koiruda <koiruda@gmail.com>
2023-02-15 01:44:31 +0900

    Add .zprofile

--- configure.sh
+++ configure.sh
@@ -8,11 +8,13 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "${XDG_CONFIG_HOME}/zsh"
 ln -snfv "${DOTPATH}/config/zsh/.zshenv" "${XDG_CONFIG_HOME}/zsh/.zshenv"
 ln -snfv "${DOTPATH}/config/zsh/.zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
+ln -snfv "${DOTPATH}/config/zsh/.zprofile" "${XDG_CONFIG_HOME}/zsh/.zprofile"
 ln -snfv "${DOTPATH}/config/zsh/.zshrc.lazy" "${XDG_CONFIG_HOME}/zsh/.zshrc.lazy"
 ln -snfv "${DOTPATH}/config/zsh/.p10k.zsh" "${XDG_CONFIG_HOME}/zsh/.p10k.zsh"
 
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshrc" "${HOME}/.zshrc"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "${HOME}/.zshenv"
+ln -sfv "$XDG_CONFIG_HOME/zsh/.zprofile" "${HOME}/.zprofile"
 
 cd "${DOTPATH}"
 
